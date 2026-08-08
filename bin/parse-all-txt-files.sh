e324c65a8841f304266261e76f57af0e2c80ebbf
Geoffrey Wu <geoffreywu1000@gmail.com>
2026-08-07 21:55:26 -0400

    pass command line args to ./bin/parse-txt-file.js

--- bin/parse-all-txt-files.sh
+++ bin/parse-all-txt-files.sh
@@ -5,5 +5,5 @@ mkdir -p output
 find packets -maxdepth 1 -type f -name '*.txt' -print0 | while IFS= read -r -d '' filename; do
     basename=$(basename "$filename")
     echo "Parsing ${basename}"
-    ./bin/parse-txt-file.js "$filename" > "output/${basename%.txt}.json"
+    ./bin/parse-txt-file.js "$filename" "$@" > "output/${basename%.txt}.json"
 done
